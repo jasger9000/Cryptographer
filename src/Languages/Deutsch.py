536b2fa6dfccea8266f78671ed359bd0e7a47963
Jasper Gerhards <jasger@outlook.de>
2022-05-25 17:32:55 +0200

    Finished German translation

---
+++ src/Languages/Deutsch.py
@@ -0,0 +1,43 @@
+Language = 'Deutsch'
+
+
+SettingsLabels = {
+    'AddLangBtn': 'Neu Sprache hinzufügen',
+    'LangLabel': 'Sprache    ',
+    'RememberKeyLabel': 'Schlüssel Merken     ',
+    'ApplyBtn': 'Änderungen übernehmen',
+}
+NewUpdateTrue = {
+    'Title': 'Neue Version verfügbar',
+    'Message': "Es steht eine neue Version zum herunterladen bereit.\n Möchten Sie diese installieren?",
+}
+NewUpdateFalse = {
+    'Title1': "Verbindung zum Server konnte nicht hergestellt werden",
+    'Message1': "Konnte nicht nach Updates suchen,\nDa keine Verbindung zu Github aufgebaut werden konnte.\nBitte versuche es später erneut, oder prüfen Sie, ob Sie mit dem Internet verbunden sind.",
+    'Title2': 'Kein Update gefunden',
+    'Message2': 'Sie verwenden derzeit die neueste Version dieser Software.',
+}
+VersionNotFound = {
+    'Title': 'Version nicht gefunden!',
+    'Message': "Die Software, die Sie derzeit verwenden, hat keine registrierte Version, bitte installieren Sie die Software neu.\nWenn Sie dies bereits getan haben, öffnen Sie bitte ein 'Issue' in meinem Github.\nhttps://wwww.github.com/jasger9000/Cryptographer"
+}
+CryptMain = {
+    'ModeMenu': 'Modus',
+    'ModeSymLabel': 'Symmetrisch',
+    'ModeAsymLabel': 'Asymmetrisch',
+    
+    'HistoryMenu': 'Verlauf',
+    
+    'HelpMenu': 'Hilfe',
+    'HelpGithubLabel': 'Github Seite Öffnen',
+    'HelpFilesLabel': 'Installationspfad öffnen',
+    'HelpSettingsLabel': 'Einstellungen',
+    'HelpAboutLabel': 'Über Cryptographer',
+    'HelpCFULabel': 'Nach Updates suchen',
+}
+SymMain = {
+
+}
+AsymMain = {
+
+}
