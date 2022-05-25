be6030436ee3126230feefb6511008defe3455a2
Jasper Gerhards <jasger@outlook.de>
2022-05-25 17:32:44 +0200

    Finished English translation

---
+++ src/Languages/English.py
@@ -0,0 +1,43 @@
+Language = 'English'
+
+
+SettingsLabels = {
+    'AddLangBtn': 'Add new Language',
+    'LangLabel': 'Language    ',
+    'RememberKeyLabel': 'Remember Key    ',
+    'ApplyBtn': 'Apply Changes'
+}
+NewUpdateTrue = {
+    'Title': 'New version available',
+    'Message': "There's a new version available for download.\n Would you like to install it?",
+}
+NewUpdateFalse = {
+    'Title1': "Couldn't connect to server",
+    'Message1': "Couldn't check for Updates,\nbecause Connection to Github Couldn't be Established.\nPlease try again later or check if you are Connected to the Internet.",
+    'Title2': 'No Update found',
+    'Message2': 'You are currently running the newest version of this Software.',
+}
+VersionNotFound = {
+    'Title': 'Version not found!',
+    'Message': "The Software you are currently using doesn't have a Version registered to it, please reinstall the Software.\nIf you have already done this please open a issue in my github."
+}
+CryptMain = {
+    'ModeMenu': 'Mode',
+    'ModeSymLabel': 'Symmetric',
+    'ModeAsymLabel': 'Asymmetric',
+    
+    'HistoryMenu': 'History',
+    
+    'HelpMenu': 'Help',
+    'HelpGithubLabel': 'Open Github Page',
+    'HelpFilesLabel': 'Open Installation Path',
+    'HelpSettingsLabel': 'Settings',
+    'HelpAboutLabel': 'About Cryptographer',
+    'HelpCFULabel': 'Check for Updates',
+}
+SymMain = {
+
+}
+AsymMain = {
+
+}
