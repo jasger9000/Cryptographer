313c061d6dbf15f9733e052a2d849639c1dce27f
Jasper Gerhards <jasger@outlook.de>
2022-06-27 19:08:24 +0200

    deleted unnecessary files

--- builder/Asym_build.bat
+++
@@ -1,4 +0,0 @@
-pyinstaller -i CryptographerIcon.ico --onefile -w python\Asym_Cryptographer.py
-move dist\Asym_Cryptographer.exe executables
-del /Q /S python\__pycache__ Asym_Cryptographer.spec build
-RMDIR /Q /S python\__pycache__ build\Asym_Cryptographer build dist 
